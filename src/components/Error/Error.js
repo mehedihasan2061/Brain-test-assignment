0d930fa4c9183f55c6f60a395521ef3ba3a15a87
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2023-03-31 01:44:36 +0600

    errorhandle

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -8,6 +8,7 @@ const Error = () => {
              <div className="flex flex-col min-h-[700px] justify-center items-center">
         <h1 className="text-4xl">Ops! An Error Ocurred!</h1>
         <br />
+        
         {error && (
           <div>
             <p className='text-3xl text-red-600'>{error.statusText || error.message}</p>
